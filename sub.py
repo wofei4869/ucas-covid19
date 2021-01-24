253dc248d82d71fe15628dc475d0eccf474ac6ac
jsphLim <jsphlim@stu2016.jnu.edu.cn>
2021-01-24 13:22:08 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -192,7 +192,7 @@ def report(username, password):
     s.headers.update(header)
 
     print(datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S %Z"))
-    for i in range(randint(3000, 3020), 0, -1):
+    for i in range(randint(5, 10), 0, -1):
         print("\r等待{}秒后填报".format(i), end='')
         sleep(1)
 
@@ -202,5 +202,4 @@ def report(username, password):
 
 
 if __name__ == "__main__":
-    while(True):
-        report(username=user, password=passwd)
+    report(username=user, password=passwd)
